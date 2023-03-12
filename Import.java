8a25ba4c1dbb9d21680212574e39c4fab08b2362
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-03-11 21:40:55 -0300

    Add files via upload
    Importando funcionalidades para o Java. Deve ser declarado antes do método main ou usando a tecla de atalho ctrl + shift + o no windows.

---
+++ Import.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		
+		// print usando import
+		
+		java.lang.String b = "Boa Noite!!";
+		System.out.println(b);
+		
+		
+		Date d = new Date();
+		System.out.println(d);
+	}
+}
